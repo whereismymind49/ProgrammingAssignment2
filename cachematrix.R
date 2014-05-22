f4d7635ca8328f46953c0fa70ff40b8cc2a4df0d
whereismymind49 <whereismymind49@gmail.com>
2014-05-22 17:34:56 -0400

    Programming Update Complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a "cache" matrix. This list of matrices is what will be
+## checked against in order to make sure that a solution does not already exist
+## and has been computed already.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)      ## This is the list "housing" the inverted
 }
 
 
-## Write a short comment describing this function
+
+
+## This function checkes the list of cache matrices checking to see if an
+## inverse of the given matrix already exists. If it does it pulls that matrix
+## inverse from the list and reports that it is doing so.
+## if that matrix solution does not already exist then it simply solves for the
+## inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {                         ## Check for existing inverted
+                message("Getting cached data.")
+                return(m)
+        }
+        data <- x$get()                     ## Solve if there does not exist on
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
